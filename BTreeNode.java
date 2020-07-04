5b29b70fdb4880edc76277e6598185927f8ec61f
RosalieCRX <55824896+rosalieCRX@users.noreply.github.com>
2020-07-04 12:21:09 -0500

    Add files via upload

---
+++ BTreeNode.java
@@ -0,0 +1,39 @@
+class BTreeNode {
+
+    /**
+     * Array of the keys stored in the node.
+     */
+    int[] keys;
+    /**
+     * Minimum degree (defines the range for number of keys)
+     **/
+    int t;
+    /**
+     * Pointers to the children, if this node is not a leaf.  If
+     * this node is a leaf, then null.
+     */
+    BTreeNode[] C;
+    /**
+     * number of key-value pairs in the B-tree
+     */
+    int n;
+    /**
+     * true when node is leaf. Otherwise false
+     */
+    boolean leaf;
+
+    /**
+     * point to other next node when it is a leaf node. Otherwise null
+     */
+    BTreeNode next;
+
+    // Constructor
+    BTreeNode(int t, boolean leaf) {
+        this.t = t;
+        this.leaf = leaf;
+        this.keys = new int[2 * t - 1];
+        this.C = new BTreeNode[2 * t];
+        this.n = 0;
+        this.next = null;
+    }
+}
